769b0801369d1e9800f7ea55a1c0b43dc980439f
dongho <dongho108@naver.com>
2023-07-26 17:30:29 +0900

    initial commit

---
+++ src/main/java/com/hongik/omechu/OmechuApplication.java
@@ -0,0 +1,13 @@
+package com.hongik.omechu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmechuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmechuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongik/omechu/OmechuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongik.omechu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmechuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
