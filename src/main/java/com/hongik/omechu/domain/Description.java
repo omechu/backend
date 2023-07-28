3ab548df7c5b7179709c8b6da42ad12e14928130
gyucheol <ggyucheol0@gmail.com>
2023-07-28 22:31:07 +0900

    feat: description string 타입으로 저장

--- src/main/java/com/hongik/omechu/domain/Description.java
+++ src/main/java/com/hongik/omechu/domain/Description.java
@@ -1,23 +1,17 @@
 package com.hongik.omechu.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import java.util.List;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity
+
 public class Description {
+    public static final List<String> VALUES = List.of(
+            "어제 저녁 찾은 메뉴 1위",
+            "오늘 점심 가장 인기 많은 메뉴",
+            "오늘 저녁 가장 인기 많은 메뉴",
+            "20대 남자가 가장 많이 찾는 음식",
+            "20대 여자가 가장 많이 찾는 음식",
+            "30대 남자가 가장 많이 찾는 음식",
+            "30대 여자가 가장 많이 찾는 음식"
+    );
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column
-    private String content;
 }
--- src/main/java/com/hongik/omechu/repository/DescriptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hongik.omechu.repository;
-
-import com.hongik.omechu.domain.Description;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DescriptionRepository extends JpaRepository<Description, Long> {
-}
